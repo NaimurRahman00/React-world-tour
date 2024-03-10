707d3406b07d0d53a48156f4c808da0dc928b03b
Naimur00 <aminaim4u@gmail.com>
2024-03-11 00:45:21 +0600

    React world tour

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Countries from "./Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Rest Countries Web</h1>
+        <Countries></Countries>
+      </div>
+    </>
+  );
+}
+
+export default App;
